eae5c79aab0614a21febca90713245133b6a28a8
Estéban Piecourt <esteban.piecourt@gmail.com>
2023-11-14 19:21:14 +0100

    🐛 (14/11) - Many improvements & optimization

--- src/fr/zebulon/stormlb/commands/StormLBCommand.java
+++ src/fr/zebulon/stormlb/commands/StormLBCommand.java
@@ -1,7 +1,6 @@
 package fr.zebulon.stormlb.commands;
 
 import fr.zebulon.stormlb.StormPlugin;
-import fr.zebulon.stormlb.api.items.ICustomItem;
 import fr.zebulon.stormlb.internal.BehaviorImpl;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -23,33 +22,17 @@ public class StormLBCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (!(sender instanceof Player)) return false;
-        Player player = (Player) sender;
         if (args.length == 0) return false;
 
-        if (args.length == 1 && args[0].equalsIgnoreCase("debug")) {
-            if (!player.hasPermission("stormlb.lucky.debug")) return false;
-
-            BehaviorImpl behavior = plugin.getBehaviorManager().getBehavior("rare");
-            if (behavior == null) return false;
-
-            ICustomItem item = behavior.getItem();
-            if (item == null) return false;
-
-            ItemStack itemStack = item.toItemStack();
-            if (itemStack == null) return false;
-
-            player.getInventory().addItem(itemStack);
-            return true;
-
-        } else if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
-            if (!player.hasPermission("stormlb.lucky.reload")) return false;
+        if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+            if (!sender.hasPermission("stormlb.lucky.reload")) return false;
 
             plugin.getConfiguration().reload();
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&aLe plugin a été rechargé avec succès !"));
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&aLe plugin a été rechargé avec succès !"));
             return true;
 
         } else if (args.length == 1 && args[0].equalsIgnoreCase("menu")) {
+            Player player = (Player) sender;
             if (!player.hasPermission("stormlb.lucky.menu")) return false;
 
             plugin.getBehaviorListener().openMenu(player);
@@ -59,7 +42,7 @@ public class StormLBCommand implements CommandExecutor {
                 && Bukkit.getPlayer(args[1]) != null
                 && plugin.getBehaviorManager().getBehavior(args[2]) != null
                 && Integer.parseInt(args[3]) > 0) {
-            if (!player.hasPermission("stormlb.lucky.give")) return false;
+            if (!sender.hasPermission("stormlb.lucky.give")) return false;
 
             Player targetPlayer = Bukkit.getPlayer(args[1]);
             if (targetPlayer == null) return false;
